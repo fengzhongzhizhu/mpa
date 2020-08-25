545af1d2624e505cb096c6395f5847dff8634037
luchaohua <luchaohua@ceair.com>
2020-08-25 14:56:58 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,15 @@ module.exports = {
   ],
   rules: {
     indent: ['error',4],
-    semi: ['error','always'],
+    semi:0,
+    eqeqeq:0,
     'no-unused-vars': ['warn'],
     'no-console': 0,
     'arrow-parens': 0,
     'generator-star-spacing': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars':0,
+    "space-before-function-paren":0,
+    'eol-last':0
   }
 };
--- config/utils.js
+++ config/utils.js
@@ -1,10 +1,29 @@
-const pageConfig = require('../page.config.js');
+var fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const glob = require('glob');
+var pageConfig = []
 
-function resolve (dir) {
+function resolve(dir) {
     return path.join(__dirname, '..', dir);
 }
+const entryFilter = glob.sync(resolve('/src/pages/***'), {});
+entryFilter.map((filePath, i) => {
+    const files = glob.sync(`${filePath}/*`, {});
+    let name = filePath.substr(filePath.lastIndexOf('/') + 1)
+    pageConfig[i] = { name }
+    files.map(file => {
+        if (path.extname(file) === '.js') {
+            pageConfig[i]['jsEntry'] = `${name}/${path.basename(file)}`
+        }
+        if (path.extname(file) === '.ejs') {
+            pageConfig[i]['html'] = `${name}/${path.basename(file)}`
+        } else if (path.extname(file) === '.html') {
+            pageConfig[i]['html'] = `${name}/${path.basename(file)}`
+        }
+    })
+});
+
 exports.pushHtmlWebpackPlugins = (webpackConfig, options = {}) => {
     if (pageConfig && Array.isArray(pageConfig)) {
         pageConfig.map(page => {
